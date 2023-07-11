bf4013fb5740f484abe78216c304a7e35b677722
Anandhita <anandhitaplank@gmail.com>
2023-07-11 20:31:39 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# twitter
+# twitter
+Test Push
---
+++ lib/apis/user_api.dart
@@ -0,0 +1,49 @@
+import 'package:appwrite/appwrite.dart';
+import 'package:appwrite/models.dart' as model;
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:fpdart/fpdart.dart';
+import '/constants/constants.dart';
+import '/core/core.dart';
+import '/models/user_model.dart';
+
+
+
+final userAPIProvider = Provider((ref) {
+  return UserAPI(db: ref.watch(appwriteDatabaseProvider));
+});
+
+abstract class IUserAPI {
+  FutureEitherVoid saveUserData(UserModel userModel);
+  Future<model.Document> getUserData(String uid);
+}
+
+class UserAPI implements IUserAPI {
+  final Databases _db;
+  UserAPI({required Databases db}) : _db = db;
+
+  @override
+  FutureEitherVoid saveUserData(UserModel userModel) async {
+    try {
+      await _db.createDocument(
+        databaseId: AppwriteConstants.databaseId,
+        collectionId: AppwriteConstants.usersCollection,
+        documentId: userModel.uid,
+        data: userModel.toMap(),
+      );
+      return right(null);
+    } on AppwriteException catch (e, stackTrace) {
+      return left(Failure(e.message ?? 'Some unexpected error', stackTrace));
+    } catch (e, stackTrace) {
+      return left(Failure(e.toString(), stackTrace));
+    }
+  }
+  
+  @override
+  Future<model.Document> getUserData(String uid) {
+    return _db.getDocument(
+      databaseId: AppwriteConstants.databaseId,
+      collectionId: AppwriteConstants.usersCollection,
+      documentId: uid,
+    );
+  }
+}
